430325557264970ade074972174d723fa838b7c2
JakubOlkowicz <olkuba@poczta.fm>
2020-05-06 19:38:19 +0200

    Add functionality changeStatus for Waiter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './redux/store.js';
+
 import MainLayout from './components/layout/MainLayout/MainLayout';
 import Homepage from './components/views/Homepage/Homepage';
 import Kitchen from './components/views/Kitchen/Kitchen';
 import Login from './components/views/Login/Login';
 import Tables from './components/views/Tables/Tables';
-import Waiter from './components/views/Waiter/Waiter';
+import Waiter from './components/views/Waiter/WaiterContainer';
 import NewOrder from './components/views/NewOrder/NewOrder';
 import Order from './components/views/Order/Order';
 import NewBooking from './components/views/NewBooking/NewBooking';
@@ -29,27 +32,29 @@ const theme = createMuiTheme({
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <StylesProvider injectFirst>
-          <ThemeProvider theme={theme}>
-            <MainLayout>
-              <Switch>
-                <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage} />
-                <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
-                <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
-                <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
-                <Route exact path={process.env.PUBLIC_URL + '/tables/booking/new'} component = {NewBooking}/>  
-                <Route exact path={process.env.PUBLIC_URL + '/tables/booking/123'} component = {Booking}/>
-                <Route exact path={process.env.PUBLIC_URL + '/tables/events/new'} component = {NewEvents}/>  
-                <Route exact path={process.env.PUBLIC_URL + '/tables/events/123'} component = {Events}/>
-                <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
-                <Route exact path={process.env.PUBLIC_URL + '/waiter/order/new'} component = {NewOrder}/>  
-                <Route exact path={process.env.PUBLIC_URL + '/waiter/order/123'} component = {Order}/>
-              </Switch>
-            </MainLayout>
-          </ThemeProvider>
-        </StylesProvider>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <StylesProvider injectFirst>
+            <ThemeProvider theme={theme}>
+              <MainLayout>
+                <Switch>
+                  <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage} />
+                  <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+                  <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
+                  <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
+                  <Route exact path={process.env.PUBLIC_URL + '/tables/booking/new'} component = {NewBooking}/>  
+                  <Route exact path={process.env.PUBLIC_URL + '/tables/booking/123'} component = {Booking}/>
+                  <Route exact path={process.env.PUBLIC_URL + '/tables/events/new'} component = {NewEvents}/>  
+                  <Route exact path={process.env.PUBLIC_URL + '/tables/events/123'} component = {Events}/>
+                  <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
+                  <Route exact path={process.env.PUBLIC_URL + '/waiter/order/new'} component = {NewOrder}/>  
+                  <Route exact path={process.env.PUBLIC_URL + '/waiter/order/123'} component = {Order}/>
+                </Switch>
+              </MainLayout>
+            </ThemeProvider>
+          </StylesProvider>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
---
+++ src/settings.js
@@ -0,0 +1,13 @@
+export const api = {
+  url: '//' + window.location.hostname + (window.location.hostname==='localhost' ? ':3131/api' : ''),
+  tables: 'tables',
+  waiter: 'waiter',
+  products: 'products',
+  order: 'order',
+  booking: 'booking',
+  event: 'event',
+  dateStartParamKey: 'date_gte',
+  dateEndParamKey: 'date_lte',
+  notRepeatParam: 'repeat=false',
+  repeatParam: 'repeat_ne=false',
+};
